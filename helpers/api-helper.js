53f2d3bc92631c8860d36839bec3472282e064b0
Matias Wengiel <matiaswengiel@gmail.com>
2024-12-13 18:01:07 -0600

    Bring parser output closer to what I will actually need

--- helpers/api-helper.js
+++ helpers/api-helper.js
@@ -17,7 +17,6 @@ const planets = {
 
 const location = "'-97.13000,+49.90000,0'";
 
-// TODO: Figure out if we can change the time from UTC to local, so that the date is more accurate
 const getUrl = (planet, location) =>
   `https://ssd.jpl.nasa.gov/api/horizons.api?format=json&MAKE_EPHEM=YES&COMMAND=${planet}&EPHEM_TYPE=OBSERVER&CENTER='coord@399'&COORD_TYPE=GEODETIC&SITE_COORD=${location}&START_TIME='${getDateToday}'&STOP_TIME='${getDateTomorrow}'&STEP_SIZE='1m'&QUANTITIES='9'&REF_SYSTEM='ICRF'&CAL_FORMAT='CAL'&CAL_TYPE='M'&TIME_DIGITS='MINUTES'&ANG_FORMAT='HMS'&APPARENT='AIRLESS'&RANGE_UNITS='AU'&SUPPRESS_RANGE_RATE='NO'&SKIP_DAYLT='NO'&SOLAR_ELONG='0,180'&EXTRA_PREC='NO'&R_T_S_ONLY='TVH'&CSV_FORMAT='NO'&OBJ_DATA='NO'`;
 
@@ -53,16 +52,21 @@ const parseResponse = (response) => {
      if (planet is up) => <planet> is currently in the sky, setting at <time> \n next rise <time>
      if (!planet is up) => <planet> is not currently in the sky, rising at <time> \n next set <time> 
   */
+
+  //If the next time the planet rises is after it next sets, it must be in the sky right now
   const isPlanetUp = rises[0] > sets[0];
   const nextSet = sets[0] > new Date() ? sets[0] : sets[1];
+  const nextRise = rises[0] > new Date() ? rises[0] : rises[1];
 
-  if (isPlanetUp) {
-    return `is currently in the sky, and will set at ${nextSet.toLocaleString()}. Next rise will be ${
-      rises[0]
-    }`;
-  }
-
-  return `will rise at ${rises[0].toLocaleString()}, and will set at ${nextSet.toLocaleString()}. It will rise again at ${rises[1].toLocaleString()}`;
+  // TODO: Maybe instead of having the date in date format we can have it as "today" or "tomorrow"
+  return {
+    verbose: isPlanetUp
+      ? `is currently in the sky, and will set ${nextSet.toLocaleString()}. Next rise will be ${rises[0].toLocaleString()}`
+      : `will rise ${rises[0].toLocaleString()}, and will set ${nextSet.toLocaleString()}. It will rise again ${rises[1].toLocaleString()}`,
+    isPlanetUp,
+    nextSet,
+    nextRise,
+  };
 };
 
 const getPlanetRiseAndSet = async (url) =>
@@ -98,7 +102,7 @@ const getPlanetRiseAndSet = async (url) =>
 for (const planet in planets) {
   const url = getUrl(planets[planet], location);
   const riseAndSet = await getPlanetRiseAndSet(url);
-  console.log(`${planet} ${riseAndSet}`);
+  console.log(`${planet} ${riseAndSet.verbose}`);
 }
 
 //Make it so that if the rise time is < timeNow, says something like "planet is already up"
